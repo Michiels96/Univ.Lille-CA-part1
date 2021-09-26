938b4cffd98c087c49759fe930fc3545421e8ab4
Thierno Souleymane BAH <bah01194151@gmail.com>
2021-09-27 00:28:56 +0200

    feat(banque sends now way less informations to users, more flexible approach)

--- banque/main.py
+++ banque/main.py
@@ -19,6 +19,13 @@ def process(cmd):
         return False, ""
 
 
+def hash(cmd):
+    ok, hashed = process(cmd)
+    if not ok:
+        return False, ""
+    return True, hashed.split(" ")[0]
+
+
 def create_banque():
     """
     """
@@ -36,41 +43,58 @@ def create_banque():
         return False
 
 
-def create_user():
+def create_client(name):
     """
     """
-    name = input("Enter your name: ")
-    ok, hashed_name = process(f"echo {name} | sha256sum")
+
+    # Hashage du nom de l'utilisateur
+    ok, hashed_name = hash(f"echo {name} | sha256sum")
     if not ok:
         return {}
-    hashed_name = hashed_name.split(" ")[0]
 
-    ok, _ = process(f"openssl genrsa -out keys/{hashed_name}.rsa 2048")
-    if not ok:
-        print("error")
-        return {}
-
+    # Génération du secret de l'utilisateur
     ok, _ = process(
-        f"echo {hashed_name} > hashed_name.tmp && openssl dgst -sha1 -sign keys/banque.rsa -out certificate.sign hashed_name.tmp && rm hashed_name.tmp")
+        f"dd if=/dev/urandom of=keys/{hashed_name}.secret bs=32 count=1")
     if not ok:
         return {}
 
-    ok, private_key = process(f"cat keys/{hashed_name}.rsa")
+    # Hashage du secret du client
+    ok, hashed_secret = hash(f"cat keys/{hashed_name}.secret | sha256sum")
     if not ok:
         return {}
 
+    # Création du certificat du client
+    ok, _ = process(
+        f"echo {name} > name.tmp && openssl dgst -sha1 -sign keys/banque.rsa -out certificate.sign name.tmp && rm name.tmp")
+    if not ok:
+        return {}
+
+    # Parsing du certificat en un format transportable
     with open("certificate.sign", "rb") as f:
         certificate = f.read()
 
     ok, _ = process(f"rm certificate.sign")
 
-    with open("user.json", "w") as f:
-        f.write(json.dumps({
-            "name": name,
-            "hashed_name": hashed_name,
-            "private_key": private_key,
-            "certificate": certificate.decode("latin-1")
-        }))
+    return {
+        "name": name,
+        "hashed_secret": hashed_secret,
+        "certificate": certificate.decode("latin-1")
+    }
+
+
+def create_shop(name):
+    """
+    """
+
+    # Récuperation de la clef publique de la banque
+    ok, banque_pub = process(f"cat keys/banque.pub")
+    if not ok:
+        return {}
+
+    return {
+        "name": name,
+        "banque_pub_key": banque_pub
+    }
 
 
 if __name__ == "__main__":
@@ -83,10 +107,20 @@ if __name__ == "__main__":
         print("2: Créer un commerçant")
         try:
             choice = int(input("Votre choix: "))
-            dir = "client" if choice == 1 else "shop"
-            create_user()
-            ok, _ = process(f"mv user.json ../{dir}/")
-            if ok:
-                print("+ Utilisateur créé avec succès")
-        except ValueError as err:
-            print(err)
+            name = input("Entrez votre nom: ")
+            if (choice > 0 and choice < 3):
+                if choice == 1:
+                    dir = "client"
+                    user = create_client(name)
+                else:
+                    dir = "shop"
+                    user = create_shop(name)
+
+                with open("user.json", "w") as f:
+                    f.write(json.dumps(user))
+
+                ok, _ = process(f"mv user.json ../{dir}/")
+                if ok:
+                    print("+ Utilisateur créé avec succès")
+        except ValueError:
+            print("Veuillez saisir soit 1 ou 2")
