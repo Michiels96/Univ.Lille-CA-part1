88a950992c729dd3086397616eedf7ddc71077a5
Thierno Souleymane BAH <thierno-souleymane.bah@leroymerlin.fr>
2021-10-04 03:25:18 +0200

    feat(shop can now send facture to client)

--- shop/main.py
+++ shop/main.py
@@ -5,6 +5,8 @@ import json
 import os
 import sys
 
+NB_BILLS = 0
+
 
 def process(cmd):
     """
@@ -18,17 +20,76 @@ def process(cmd):
         return False, ""
 
 
+def verify_signature(signature, message):
+    """
+    """
+    # Convertir le certificat en binaire
+    with open("tmp.sign", "wb") as f:
+        f.write(signature.encode("latin-1"))
+
+    # Vérification du certificat du client
+    ok, _ = process(
+        f"(echo {message} > from.tmp && openssl dgst -sha1 -verify keys/banque.pub -signature tmp.sign from.tmp)")
+
+    _, _ = process("rm from.tmp tmp.sign")
+
+    return ok
+
+
+def is_bill_exists(bill_number):
+    """
+    """
+    try:
+        os.listdir("checks").index(str(bill_number))
+        return True
+    except ValueError:
+        return False
+
+
 def init():
     """
     """
-    ok, _ = process("mkdir -p checks rejected accepted")
+    ok, _ = process("mkdir -p checks rejected accepted bills")
     return ok
 
 
 def clean():
     """
     """
-    ok, _ = process("rm -rf checks rejected accepted")
+    ok, _ = process("rm -rf checks rejected accepted bills")
+    return ok
+
+
+def send_bill(user, amount):
+    """
+    """
+    global NB_BILLS
+    NB_BILLS += 1
+
+    # Création de la facture
+    with open("facture.json", "w") as f:
+        f.write(json.dumps({
+            "shop_name": user["name"],
+            "amount": amount,
+            "bill_number": NB_BILLS,
+            "shop_signature": user["signature"]
+        }))
+
+    # Hashage de la facture + secret du commercant
+    ok, hashed = process(
+        f"(cat facture.json && echo {user['hashed_secret']}) | sha256sum")
+    if not ok:
+        return False
+    hashed = hashed.split(" ")[0]
+
+    # Archivage de la facture
+    ok, _ = process(f"echo {hashed} > bills/{NB_BILLS}")
+    if not ok:
+        return False
+
+    # Envoi de la facture au client
+    ok, _ = process(f"mv facture.json ../client/bills/{hashed}.json")
+
     return ok
 
 
@@ -40,17 +101,37 @@ def verify_check(user, path):
     with open(path, "r") as f:
         check = json.load(f)
 
-    # Convertir le certificat en binaire
-    with open("tmp.sign", "wb") as f:
-        f.write(check["certificate"].encode("latin-1"))
-
     # Vérification du certificat du client
+    if not verify_signature(check["signature"], check['from']):
+        return False
+
+    # Verification de l'existence de la facture
+    if not is_bill_exists(check["bill_number"]):
+        return False
+
+   # Formation de la facture à partir du check
+    with open("facture.json", "w") as f:
+        f.write(json.dumps({
+            "shop_name": check["shop_name"],
+            "amount": check["amount"],
+            "bill_number": check["bill_number"],
+            "shop_signature": check["shop_signature"]
+        }))
+
+    # Hashage de la facture + secret du commercant
+    ok, hashed = process(
+        f"(cat facture.json && echo {user['hashed_secret']}) | sha256sum")
+    if not ok:
+        return False
+    hashed = hashed.split(" ")[0]
+
+    # Vérification de la facture reconstituée
     ok, _ = process(
-        f"(echo {check['from']} > from.tmp && openssl dgst -sha1 -verify ../bank/keys/bank.pub -signature tmp.sign from.tmp)")
+        f"echo {hashed} > hashed.tmp && diff hashed.tmp bills/{check['bill_number']}")
 
-    _, _ = process("rm from.tmp tmp.sign")
+    _, _ = process(f"rm hashed.tmp")
 
-    return ok and user["name"] == check["to"]
+    return ok
 
 
 def verify_checks(user):
@@ -105,6 +186,11 @@ if __name__ == "__main__":
         ok = verify_checks(user)
         print(
             "+ Vérification faites avec succès") if ok else print("- Chèques non vérifiés")
-    elif sys.argv[1] == "send":
+
+    elif sys.argv[1] == "checks":
         ok = send_checks()
         print("+ Chèques envoyés avec succès") if ok else print("- Chèques non envoyés")
+
+    elif sys.argv[1] == "bill":
+        ok = send_bill(user, int(sys.argv[2]))
+        print("+ Facture envoyée avec succès") if ok else print("- Facture non envoyée")
